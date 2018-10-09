b638bb7b73d49126de9c5bb75faf22224500ef65
ElStickers <30716143+ElStickers@users.noreply.github.com>
2018-10-09 01:53:56 -0700

    Item Selection Display

--- csci10/project2_final/asm_p2.asm
+++ csci10/project2_final/asm_p2.asm
@@ -20,22 +20,35 @@ _main:
 	and		esp, 0xfffffff0
 
 	_begin:
-		callSystem	clear
-		print		ascii1
-		print		ascii2
+		callSys		clear
+		print			ascii1
+		print			ascii2
 		print 		ascii3
 		print 		ascii4
 		print 		ascii5
 
-		print		line
+		print				line
 		printGuide	guide_frmt, guide1
-		print 		line
+		print 			line
 		printItem 	item0, item1, item2, item3, item4, item5
-		print		line
+		print				line
 
 		print 		item_prmpt
-		scan		uint_frmt, item_input
+		scan			uint_frmt, item_input
 
+		mov 			al, [item_input]
+		cmp				al, 0
+		je				_exit
+
+		mov				al, [item_input]
+		cmp				al, 1
+		je				_output1
+
+	_output1:
+		outputItem		itm_frmt, item0
+		outputPrice		price_frmt, item0
+
+	_exit:
 
 	mov		esp, ebp
 	mov		eax, 1
