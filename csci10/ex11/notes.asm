493f370efd48f079c1570edf0790824bb618a775
Stickers <Stick@DESKTOP-8EM820U>
2018-10-29 10:38:26 -0700

    woopps

---
+++ csci10/ex11/notes.asm
@@ -0,0 +1,13 @@
+working with arrays on x86
+access a specific element
+  #byte per element:
+    scale
+    offset
+    base
+      [base+offset*scale]
+
+locating min/max values in an array
+  greddy algorithms
+    assume the first value is the largest
+    prove yourself wrong (or not)
+///////////////////ELIXIR?///////////////////////
