512f689543f97d655e5bf5e6a367f98638578ae3
Stickers <zhouyun56@gmail.com>
2018-12-12 09:35:04 -0800

    .

---
+++ csci21/hashtables/notes.hpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using std::cout;
+using std::endl;
+using std::string;
+
+unsigned int hashBySize(string s) {
+  return s.size();
+}
+
+int int main(int argc, char const *argv[]) {
+  string test[] = {"apple", "orange", "banana", " "
+  return 0;
+}
